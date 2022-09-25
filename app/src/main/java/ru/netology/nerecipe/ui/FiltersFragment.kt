3ccc16a01ad34cd07075792a218cb1fff65aa6b9
Lol-ice <lolice@mail.ru>
2022-09-25 15:15:19 +0300

    1

--- app/src/main/java/ru/netology/nerecipe/ui/FiltersFragment.kt
+++ app/src/main/java/ru/netology/nerecipe/ui/FiltersFragment.kt
@@ -25,29 +25,29 @@ class FiltersFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
-       container: ViewGroup?,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return FragmentFiltersBinding.inflate(layoutInflater, container, false)
-           .also { binding ->
+            .also { binding ->
 
                 val sharedPref =
                     activity?.getSharedPreferences(SHARED_PREFS_KEY, Context.MODE_PRIVATE)
 
-               viewModel.listOfFilters.observe(viewLifecycleOwner) { listOfFilters ->
+                viewModel.listOfFilters.observe(viewLifecycleOwner) { listOfFilters ->
                     binding.filtersAllCategoryCheckBox.isChecked = listOfFilters.isEmpty()
                     binding.filtersAllCategoryCheckBox.isClickable = false
                 }
 
                 val listOfCategoryNames = mutableListOf<String>()
-               for (i in 0 until Categories.values().size) {
-                   listOfCategoryNames.add(Categories.values()[i].categoryName)
-               }
+                for (i in 0 until Categories.values().size) {
+                    listOfCategoryNames.add(Categories.values()[i].categoryName)
+                }
 
                 val filtersAdapter = FiltersAdapter(viewModel, sharedPref, viewLifecycleOwner)
-               binding.filtersRecyclerView.adapter = filtersAdapter
+                binding.filtersRecyclerView.adapter = filtersAdapter
 
-               viewModel.data.observe(viewLifecycleOwner) {
+                viewModel.data.observe(viewLifecycleOwner) {
                     filtersAdapter.submitList(listOfCategoryNames)
                 }
 
@@ -60,14 +60,14 @@ class FiltersFragment : Fragment() {
                     if (sharedPref != null) {
                         with(sharedPref.edit()) {
                             for (i in 0 until Categories.values().size) {
-                               putBoolean(i.toString(), false)
+                                putBoolean(i.toString(), false)
 
 
-                           }
+                            }
                             apply()
                         }
                     }
-                   viewModel.onResetFiltersButtonClicked()
+                    viewModel.onResetFiltersButtonClicked()
                     viewModel.filterCheckboxUpdate.value = true
                 }
 
@@ -77,82 +77,82 @@ class FiltersFragment : Fragment() {
                         binding.applyFilterButton.performClick()
                     }
                 }
-               requireActivity().onBackPressedDispatcher.addCallback(
-                   onBackPressedCallback
-               )
+                requireActivity().onBackPressedDispatcher.addCallback(
+                    onBackPressedCallback
+                )
 
             }.root
 
 
     }
     class FilterFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = FragmentFiltersBinding.inflate(layoutInflater).also { binding ->
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ) = FragmentFiltersBinding.inflate(layoutInflater).also { binding ->
 
-        binding.okFilterButton.setOnClickListener {
-            onOkFilterButtonClicked(binding)
-            //findNavController().navigateUp()
-        }
-    }.root
+            binding.okFilterButton.setOnClickListener {
+                onOkFilterButtonClicked(binding)
+                //findNavController().navigateUp()
+            }
+        }.root
 
         companion object {
             const val SHARED_PREFS_KEY = "ru.netology.nerecipe.PREFERENCE"
         }
 
-    private fun onOkFilterButtonClicked(binding: FragmentFiltersBinding) {
+        private fun onOkFilterButtonClicked(binding: FragmentFiltersBinding) {
 
-        var numberOfCheckedCategories = 0
+            var numberOfCheckedCategories = 0
 
-        val listOfCategoryNames : ArrayList<String> = ArrayList()
+            val listOfCategoryNames : ArrayList<String> = ArrayList()
 
-        if (binding.category_European.isChecked) {
-            listOfCategoryNames.add(binding.category_European.text.toString())
-            numberOfCheckedCategories++
+            if (binding.category_European.isChecked) {
+                listOfCategoryNames.add(binding.category_European.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (binding.category_Asian.isChecked) {
+                listOfCategoryNames.add(binding.category_Asian.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (binding.category_PanAsian.isChecked) {
+                listOfCategoryNames.add(binding.category_PanAsian.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (binding.category_Oriental.isChecked) {
+                listOfCategoryNames.add(binding.category_Oriental.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (binding.category_American.isChecked) {
+                listOfCategoryNames.add(binding.category_American.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (binding.category_Russian.isChecked) {
+                listOfCategoryNames.add(binding.category_Russian.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (binding.category_Mediterranean.isChecked) {
+                listOfCategoryNames.add(binding.category_Mediterranean.text.toString())
+                numberOfCheckedCategories++
+            }
+
+            if (numberOfCheckedCategories == 0) {
+                Toast.makeText(requireContext(), "Please choose at least one filter", Toast.LENGTH_LONG)
+                    .show()
+
+            }
+            val resultBundle = Bundle(1)
+            resultBundle.putStringArrayList("listOfChosenCategories", listOfCategoryNames)
+            setFragmentResult("chosenCategories", resultBundle)
+            findNavController().popBackStack()
         }
-
-        if (binding.category_Asian.isChecked) {
-            listOfCategoryNames.add(binding.category_Asian.text.toString())
-            numberOfCheckedCategories++
-        }
-
-        if (binding.category_PanAsian.isChecked) {
-            listOfCategoryNames.add(binding.category_PanAsian.text.toString())
-            numberOfCheckedCategories++
-        }
-
-        if (binding.category_Oriental.isChecked) {
-            listOfCategoryNames.add(binding.category_Oriental.text.toString())
-            numberOfCheckedCategories++
-        }
-
-        if (binding.category_American.isChecked) {
-            listOfCategoryNames.add(binding.category_American.text.toString())
-            numberOfCheckedCategories++
-        }
-
-        if (binding.category_Russian.isChecked) {
-            listOfCategoryNames.add(binding.category_Russian.text.toString())
-            numberOfCheckedCategories++
-        }
-
-        if (binding.category_Mediterranean.isChecked) {
-            listOfCategoryNames.add(binding.category_Mediterranean.text.toString())
-            numberOfCheckedCategories++
-        }
-
-        if (numberOfCheckedCategories == 0) {
-            Toast.makeText(requireContext(), "Please choose at least one filter", Toast.LENGTH_LONG)
-                .show()
-
-        }
-        val resultBundle = Bundle(1)
-        resultBundle.putStringArrayList("listOfChosenCategories", listOfCategoryNames)
-        setFragmentResult("chosenCategories", resultBundle)
-        findNavController().popBackStack()
-    }
 
     }
 }
